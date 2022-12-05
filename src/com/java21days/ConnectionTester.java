64fd3ca5737b69331b51acedfa60f1d7508148c3
Alejandro Salas <asalas.wd@outlook.com>
2022-12-04 19:03:19 -0500

    Complete try statement

--- src/com/java21days/ConnectionTester.java
+++ src/com/java21days/ConnectionTester.java
@@ -2,13 +2,16 @@ package com.java21days;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class ConnectionTester extends JFrame {
-    Dimension dim = new Dimension(250, 120);
+    Dimension dim = new Dimension(550, 120);
     String text = "Checking connection...";
 
     ConnectionTester(String url){
@@ -16,18 +19,23 @@ public class ConnectionTester extends JFrame {
         setSize(dim);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        JLabel label = new JLabel(text);
-        add(label);
         try {
             URL page = new URL(url);
             HttpURLConnection connection = (HttpURLConnection) page.openConnection();
+            String  response = connection.getResponseMessage();
+            text = response;
+            InputStreamReader in = new InputStreamReader((InputStream) connection.getContent());
+            BufferedReader buffer = new BufferedReader(in);
 
         } catch (MalformedURLException e){
             System.out.println("Malformed URL Exception: " + e.getMessage());
         } catch (IOException e){
             System.out.println("I/O Exception: " + e.getMessage());
+            text = e.getMessage();
         }
 
+        JLabel label = new JLabel(text);
+        add(label);
         setVisible(true);
     }
 
